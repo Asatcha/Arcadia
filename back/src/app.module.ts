43ae3f4836fe957c4194247f4b0a22ac42851825
Asatcha <pauline.blain@hotmail.fr>
2025-01-08 17:26:33 +0100

    feat(auth): create auth template

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -6,6 +6,7 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    AuthModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
---
+++ back/src/auth/auth.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+
+// localhost:3000/login
+@Controller('login')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  create(@Body() loginDto: LoginDto) {
+    return this.authService.create(loginDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.authService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.authService.findOne(+id);
+  }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
+  //   return this.authService.update(+id, updateAuthDto);
+  // }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.authService.remove(+id);
+  }
+}
