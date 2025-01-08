41e7353154c427a5641e6a7c8eddfa6f3c178788
Asatcha <pauline.blain@hotmail.fr>
2025-01-08 16:55:37 +0100

    fix(module): add missing controller & service

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -14,5 +17,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       synchronize: true,
     }),
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
