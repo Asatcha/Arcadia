139731308772291d196ff39707f568d8ad82583d
Asatcha <pauline.blain@hotmail.fr>
2025-02-02 18:32:02 +0100

    feat(menu): create menu

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MenuComponent } from './components/menu/menu.component';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'arcadia-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, MenuComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'front';
 }
