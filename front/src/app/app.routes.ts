d2e19a87c6ee0554f62be20fd23fa38c0d00bebc
Asatcha <pauline.blain@hotmail.fr>
2025-02-08 17:17:01 +0100

    feat(login): create login component

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
     { path: '**', redirectTo: 'home' },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
