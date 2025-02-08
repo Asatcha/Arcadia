f2efe79b424bb27070d953b5c511064e187ca1f2
Asatcha <pauline.blain@hotmail.fr>
2025-02-08 17:17:01 +0100

    feat(login): create login component

---
+++ front/src/app/interceptors/base-url.interceptor.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptorFn, HttpRequest, HttpHandlerFn, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
+
+@Injectable()
+export class BaseUrlInterceptor {
+  static intercept: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> => {
+    const baseUrl = environment.apiUrl;
+
+    const clonedRequest = req.clone({ url: `${baseUrl}${req.url}` });
+    return next(clonedRequest);
+  };
+}
