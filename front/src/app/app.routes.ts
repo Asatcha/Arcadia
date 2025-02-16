1acf7d9020d5fc114d35683c79274f08c43f49e2
Asatcha <pauline.blain@hotmail.fr>
2025-02-16 15:53:09 +0100

    feat(habitat): create habitat page

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AdminComponent } from './pages/admin/admin.component';
 import { AuthGuard } from './guards/auth.guard';
+import { HabitatComponent } from './pages/habitat/habitat.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'admin', component: AdminComponent, canActivate: [AuthGuard], data: { roles: ['isAdmin']} },
   { path: 'home', component: HomeComponent },
+  { path: 'habitat', component: HabitatComponent },
   { path: '**', redirectTo: 'home' },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
